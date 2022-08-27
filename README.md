d66d26a2f52ab60bef1e29d04cac27348e54b762
ChrisCODES23 <christiantalledo89@gmail.com>
2022-08-27 00:16:20 -0700

    Did some finishing touches

--- README.md
+++ README.md
@@ -3,12 +3,20 @@
 ## JAVASCRIPT
 
 - Created a time variable for moment.
-- A logical loop that examines whether the time is past present or future.
--
--
+- Created a logical loop that examines whether the time is past present or future.
+- Created a function that runs when user clicked savebtn. It also sets the current value of text and time to the local storage.
+- Created a Bunch of getItem codes to get the item from the local storage.
 
 ## USAGE
 
+<img src = "ezgif.com-gif-maker.gif">
+
 ## LINKS
 
+Github link : https://github.com/ChrisCODES23/daily-tracker
+
+Live link:
+
 ## CREDITS
+
+    Coded by Christian Talledo
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-//  Time
+// ? Time Variable to store moments
 var timeToday = moment()
 $("#1a").text(timeToday.format("MMM Do YYYY, h:m:ss"));
 
 $(document).ready(function () {
-    var currentHour = moment().format("H")
+    var currentHour = moment().format("H");
 
 
     // ? A logical loop that examines whether the time is past present or future.
@@ -20,47 +20,29 @@ $(document).ready(function () {
             textAreaEl.attr("class", "col-9 future")
         }
     }
-    // ? Runs when user clicks the save button
+    // ? Runs when user clicked savebtn. It also sets the current value of text and time to the local storage. 
     $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".time-block").val();
+        console.log(this);
+        var text = $(this).siblings("textarea").val();
         var time = $(this).parent().attr("id");
-        localStorage.setItem(text, time);
+        console.log(text)
+        console.log(time)
 
+        localStorage.setItem(time, text);
     });
 
+    // ? Bunch of getItem to get the item from the local storage
 
-    $("#hour9 .time-block").val(localStorage.getItem("hour9"));
-    $("#hour10 .time-block").val(localStorage.getItem("hour10"));
-    $("#hour11 .time-block").val(localStorage.getItem("hour11"));
-    $("#hour12 .time-block").val(localStorage.getItem("hour12"));
-    $("#hour13 .time-block").val(localStorage.getItem("hour13"));
-    $("#hour14 .time-block").val(localStorage.getItem("hour14"));
-    $("#hour15 .time-block").val(localStorage.getItem("hour15"));
-    $("#hour16 .time-block").val(localStorage.getItem("hour16"));
-    $("#hour17 .time-block").val(localStorage.getItem("hour17"));
+    $("#hour9 textarea").val(localStorage.getItem("hour9"));
+    $("#hour10 textarea").val(localStorage.getItem("hour10"));
+    $("#hour11 textarea").val(localStorage.getItem("hour11"));
+    $("#hour12 textarea").val(localStorage.getItem("hour12"));
+    $("#hour13 textarea").val(localStorage.getItem("hour13"));
+    $("#hour14 textarea").val(localStorage.getItem("hour14"));
+    $("#hour15 textarea").val(localStorage.getItem("hour15"));
+    $("#hour16 textarea").val(localStorage.getItem("hour16"));
+    $("#hour17 textarea").val(localStorage.getItem("hour17"));
 });
-
-
-
-
-        // function saveToLocal() {
-        //     var value = $(this).prev().val()
-        //     console.log(value)
-        //     localStorage.setItem(value, "savedInput");
-        //     var userInput = localStorage.getItem(value);
-        //     textAreaEl.textContent = userInput;
-        // }
-
-
-
-    //     function renderLastSaved() {
-    //         var userInput = localStorage.getItem(saved);
-    //         textAreaEl.textContent = userInput;
-    //     }
-    //     renderLastSaved();
-
-    // })
-
 
 
 
