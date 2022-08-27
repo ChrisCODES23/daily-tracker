fc974148cfd83a80acc39b072ea45aeb97028e36
ChrisCODES23 <christiantalledo89@gmail.com>
2022-08-26 22:02:40 -0700

    Set up localstorage to be able to set and get the value stored in it

--- script.js
+++ script.js
@@ -2,41 +2,66 @@
 //  Time
 var timeToday = moment()
 $("#1a").text(timeToday.format("MMM Do YYYY, h:m:ss"));
-var currentHour = moment().format("H")
 
-var textarea = $('textarea')
+$(document).ready(function () {
+    var currentHour = moment().format("H")
 
-var timeBlock = $('time-block')
-// ? A logical loop that examines whether the time is past present or future.
-for (i = 9; i < 18; i++) {
-    var textAreaEl = $("#" + i).prev();
-    if (parseInt(currentHour) == i) {
-        textAreaEl.attr("class", "col-9 present")
+
+    // ? A logical loop that examines whether the time is past present or future.
+    for (i = 9; i < 18; i++) {
+        var textAreaEl = $("#" + i).prev();
+        if (parseInt(currentHour) == i) {
+            textAreaEl.attr("class", "col-9 present")
+        }
+        else if (parseInt(currentHour) > i) {
+            textAreaEl.attr("class", "col-9 past")
+        }
+        else if (parseInt(currentHour) < i) {
+            textAreaEl.attr("class", "col-9 future")
+        }
     }
-    else if (parseInt(currentHour) > i) {
-        textAreaEl.attr("class", "col-9 past")
-    }
-    else if (parseInt(currentHour) < i) {
-        textAreaEl.attr("class", "col-9 future")
-    }
+    // ? Runs when user clicks the save button
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings("textarea").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(text, time);
 
-    $("#" + i).on("click", saveToLocal)
-}
-// ? Runs when user clicks the save button
-function saveToLocal() {
-    var value = $(this).prev().val();
-    var time = $(this).parent().attr("id");
-    console.log(value)
-    localStorage.setItem(time, value);
+    });
 
-}
 
-function renderLastSaved() {
-    var userInput = localStorage.getItem(value);
-    textAreaEl.textContent = userInput;
-}
-renderLastSaved();
+    $("#hour9 .textarea").val(localStorage.getItem("hour9"));
+    $("#hour10 .textarea").val(localStorage.getItem("hour10"));
+    $("#hour11 .textarea").val(localStorage.getItem("hour11"));
+    $("#hour12 .textarea").val(localStorage.getItem("hour12"));
+    $("#hour13 .textarea").val(localStorage.getItem("hour13"));
+    $("#hour14 .textarea").val(localStorage.getItem("hour14"));
+    $("#hour15 .textarea").val(localStorage.getItem("hour15"));
+    $("#hour16 .textarea").val(localStorage.getItem("hour16"));
+    $("#hour17 .textarea").val(localStorage.getItem("hour17"));
+});
 
+
+
+
+
+
+    // function saveToLocal() {
+    //     var value = $(this).prev().val()
+    //     console.log(value)
+    //     localStorage.setItem(value, "savedInput");
+    //     var userInput = localStorage.getItem(value);
+    //     textAreaEl.textContent = userInput;
+    // }
+
+
+
+//     function renderLastSaved() {
+//         var userInput = localStorage.getItem(saved);
+//         textAreaEl.textContent = userInput;
+//     }
+//     renderLastSaved();
+
+// })
 
 
 
